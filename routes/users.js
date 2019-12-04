acffbd7ff8f21c53cf8004acb7fd4fe0e2cae0a7
Reinhard Liess <reinhard.liess@gmail.com>
2019-12-04 18:15:46 +0100

    add 'add user' route

--- routes/users.js
+++ routes/users.js
@@ -14,8 +14,25 @@ router.get('/', asyncHandler(authenticateUser), asyncHandler(async (req, res) =>
 }));
 
 // POST - add a new user
-router.post('/', asyncHandler(async (req, res) => {
+router.post('/', asyncHandler(async (req, res, next) => {
 
+  try {
+    // Get the user from the request body.
+    await User.create(req.body);
+    res.location('/');
+    // Set the status to 201 Created and end the response.
+    return res.status(201).end();
+  } catch (error) {
+    // // checking for validation error
+    // if (error.name === "SequelizeValidationError") {
+    //   // const book = await Book.build(req.body);
+    //   // res.render("new-book", { book, errors: error.errors, title: "New Book" })
+    //   console.log(error.errors);
+    // } else {
+    //   next(error);
+    // }
+    res.status(400).json(error);
+  }
 }));
 
 module.exports = router;
